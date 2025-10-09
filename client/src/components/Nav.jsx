6295362fe7e48c0fa941377a4eb481c5c5e59c12
Brad Hollis <bradleywhollis@gmail.com>
2025-10-09 17:59:08 -0400

    Added a logo and formatted Nav Colors

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,14 +1,21 @@
 import { NavLink } from 'react-router-dom';
 
+/*
 const linkStyle = ({ isActive }) => ({
   opacity: isActive ? 1 : 0.75,
 });
-
+*/
+const linkStyle = ({ isActive }) => ({
+  color: isActive ? "#333" : "#777", // dark gray when active, lighter gray otherwise
+  textDecoration: isActive ? "underline" : "none", // underline only when active
+});
 export default function Nav() {
   return (
     <nav className="nav">
       <div className="container nav__row">
-        <div className="logo">Schick &amp; Associates</div>
+        <div className="logo">
+          <img src="/Schick Logo.png" alt="Schick & Associates" className="logo-img" />
+        </div>
 
         {/* This wrapper is required so gap/align rules take effect */}
         <div className="nav__links">
