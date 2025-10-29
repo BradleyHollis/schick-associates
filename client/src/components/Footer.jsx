4bbe4d38f548a9e35d6183090ee6d9aa924ad9af
Brad Hollis <bradleywhollis@gmail.com>
2025-10-29 13:39:57 -0400

    Added minor adjustments to about page and grammar errors in footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -3,7 +3,7 @@ export default function Footer() {
   return (
     <footer className="footer" role="contentinfo">
       <div className="container footer__inner">
-        © 2025 <strong>Schick &amp; Associates</strong> • Making Friends is our Business
+        © 2025 <strong>Schick &amp; Associates</strong> • Making Friends is Our Business
       </div>
     </footer>
   );
